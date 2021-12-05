d65bd90e7599a40b5b7ca54aaf053ad0da6e51dd
Guilherme Goncalves <grgoncal@gmail.com>
2021-12-05 17:49:16 -0300

    Add project files.

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiRedisConnector.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection serviceCollection)
+        {
+
+
+            return serviceCollection;
+        }
+    }
+}
+ 
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace MultiRedisConnector
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            var buider = new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
+                                                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                                                  .AddEnvironmentVariables();
+
+            buider.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDependencies();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
